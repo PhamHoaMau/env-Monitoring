b898fa85b38c59e88156fa918a777eec27739ce9
PhamHoaMau <hoamau250598@gmail.com>
2020-08-26 10:56:08 +0700

    25.08.2020 update 6

--- test-network/genCryptoForOrg2.sh
+++ test-network/genCryptoForOrg2.sh
@@ -1,6 +1,4 @@
 ORG=$1
-export FABRIC_CFG_PATH=$PWD/configtx/
-export PATH=$PWD/../bin:$PATH
 /bin/bash ./organizations/fabric-ca/registerEnroll.sh 
 echo " "
 echo "#########  Generating CCP file ##############"
@@ -20,13 +18,7 @@ configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/m
 echo " "
 echo "######### Create channeltx ##########"
 echo " "
-if [ $ORG == 1 ]; then
-    configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID mychannel -asOrg Org1MSP
-elif [ $ORG == 2 ]; then
-    configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID mychannel -asOrg Org2MSP
-else
-    echo "Orderer Org"
-fi
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID mychannel -asOrg Org2MSP
 echo " "
 echo "#########  Done. ##############"
 echo " "
