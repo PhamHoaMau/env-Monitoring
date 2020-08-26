e29b1c7942ba43e9648c49c23c273b8df2f0036d
PhamHoaMau <hoamau250598@gmail.com>
2020-08-26 13:15:05 +0700

    25.08.2020 update 9

--- test-network/organizations/ccp-generate.sh
+++ test-network/organizations/ccp-generate.sh
@@ -4,7 +4,7 @@ function one_line_pem {
     echo "`awk 'NF {sub(/\\n/, ""); printf "%s\\\\\\\n",$0;}' $1`"
 }
 
-function json_ccp1 {
+function json_ccp {
     local PP=$(one_line_pem $4)
     local CP=$(one_line_pem $5)
     sed -e "s/\${ORG}/$1/" \
@@ -12,18 +12,7 @@ function json_ccp1 {
         -e "s/\${CAPORT}/$3/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
-        organizations/ccp-template-org1.json
-}
-
-function json_ccp2 {
-    local PP=$(one_line_pem $4)
-    local CP=$(one_line_pem $5)
-    sed -e "s/\${ORG}/$1/" \
-        -e "s/\${P0PORT}/$2/" \
-        -e "s/\${CAPORT}/$3/" \
-        -e "s#\${PEERPEM}#$PP#" \
-        -e "s#\${CAPEM}#$CP#" \
-        organizations/ccp-template-org2.json
+        organizations/ccp-template.json
 }
 
 function yaml_ccp {
@@ -39,18 +28,18 @@ function yaml_ccp {
 
 ORG=1
 P0PORT=7051
-CAPORT=32769
+CAPORT=7054
 PEERPEM=organizations/peerOrganizations/org1.jwclab.com/tlsca/tlsca.org1.jwclab.com-cert.pem
 CAPEM=organizations/peerOrganizations/org1.jwclab.com/ca/ca.org1.jwclab.com-cert.pem
 
-echo "$(json_ccp1 $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.jwclab.com/connection-org1.json
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.jwclab.com/connection-org1.json
 echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.jwclab.com/connection-org1.yaml
 
 ORG=2
 P0PORT=9051
-CAPORT=32769
+CAPORT=8054
 PEERPEM=organizations/peerOrganizations/org2.jwclab.com/tlsca/tlsca.org2.jwclab.com-cert.pem
 CAPEM=organizations/peerOrganizations/org2.jwclab.com/ca/ca.org2.jwclab.com-cert.pem
 
-echo "$(json_ccp2 $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org2.jwclab.com/connection-org2.json
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org2.jwclab.com/connection-org2.json
 echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org2.jwclab.com/connection-org2.yaml
